58a3111421d3ce1571f120e417fd7d2082d5a717
RPAPE3 <106628145+RPAPE3@users.noreply.github.com>
2022-06-17 14:40:02 -0700

    removed comments

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Assignment code here
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// >>>>>>>>>WHEN prompted for the length of the password
-// >>>>>>>>>THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
 var passwordOptions = ["abcdefghijklmopqrstuvxyz", "ABCDEFGHIJKLMOPQRSTUVWXYZ", "0123456789", "!@#$%^&*"];
 
 function promptPassword() {
@@ -73,22 +56,4 @@ function generatePassword(numCharacters, passwordList) {
   }
   return password;
 };
-
-
-
-
-// // // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // // // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// };
-
-// // // // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword());
 
